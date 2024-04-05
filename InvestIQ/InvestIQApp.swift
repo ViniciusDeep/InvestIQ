9ec5517c990a9bd76f5da214460eae80f818a3d5
Vinicius.Correia1 <vinicius.correia@wendys.com>
2024-04-05 19:55:21 -0300

    Initial Commit

---
+++ InvestIQ/InvestIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestIQApp.swift
+//  InvestIQ
+//
+//  Created by Vinicius Mangueira on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InvestIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
